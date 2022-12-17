7300e9528f2545ae5c55c628467348ed94497556
kmasuda1986 <kentarou_masuda@hacklab-tech.io>
2022-12-17 12:08:04 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# SmartContract Templates
+
+## Installation
+
+```bash
+yarn install
+```
+
+## Usage
+
+### ERC20 Template
+
+```bash
+hoge
+```
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.ts
+```
---
+++ test/ERC20Template.ts
@@ -0,0 +1,31 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("ERC20Template", () => {
+  const deployFixture = async () => {
+    const [
+      owner,
+      account1
+    ] = await ethers.getSigners();
+
+    const ERC20Template = await ethers.getContractFactory("ERC20Template");
+    const contract = await ERC20Template.deploy();
+
+    return { contract, owner, account1 };
+  }
+
+  describe("Functions: mint", () => {
+    describe("Success", () => {
+      it("発行された通貨が、残高に反映されていること", async () => {  
+        const { contract, account1 } = await loadFixture(deployFixture);
+
+        const mintAmount = ethers.BigNumber.from(1000000);
+        await contract.mint(account1.address, mintAmount)
+  
+        expect(await contract.balanceOf(account1.address)).to.equal(mintAmount);
+      });
+    })
+  });
+});

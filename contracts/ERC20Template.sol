13b766499f582aace829b66de05f85cf5d304a95
kmasuda1986 <kentarou_masuda@hacklab-tech.io>
2022-12-17 14:29:35 +0900

    fix: ERC20Template

--- contracts/ERC20Template.sol
+++ contracts/ERC20Template.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @title ERC20 Template contract
+/// @title ERC20 template
 /// @author Kentaro Masuda
 contract ERC20Template is ERC20, Ownable {
     uint8 private _desimals = 18;
